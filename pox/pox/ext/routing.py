14f54e3304c3abf5d741ec9f592f24e54759c2eb
sktollman <stollman@stanford.edu>
2018-04-30 20:26:56 -0700

    Figure 9
    Adds code to generate figure 9. Takes about 1.5 mins to run.
    Cleans up jellyfish topology generator so that it doesn't take
    a ridiculous amount of time to run. Adds code to the routing class
    to generate paths for random permutation traffic and count the number
    of times each link is crossed in the paths. Found Yen's networkx
    implementation and modifies it so that it actually works.
    
    Running `python figure9.py` generates the graph and saves it to `figure9.png`

--- pox/pox/ext/routing.py
+++ pox/pox/ext/routing.py
@@ -1,25 +1,40 @@
+#!/usr/bin/python
+
 from collections import defaultdict
+from functools import partial
 import networkx as nx
+import itertools
+import random
+
+import yens
 
 class Routing():
     def __init__(self, topo, rproto):
-        path_fn = None
+        self.topo = topo
+        self.set_path_fn(rproto)
 
-        if rproto == 'kshort':
-            path_fn = self.k_shortest_paths
-        elif rproto == 'ecmp':
-            path_fn = self.ecmp_paths
+    def set_path_fn(self, rproto):
+        self.path_fn = None
 
-        if not path_fn: raise Exception('Unknown routing protocol')
+        if rproto == 'kshort': # use 8 as k
+            self.path_fn = partial(self.k_shortest_paths, k=8)
+        elif rproto == 'ecmp8':
+            self.path_fn = partial(self.ecmp_paths, k=8)
+        elif rproto == 'ecmp64':
+            self.path_fn = partial(self.ecmp_paths, k=64)
 
+        if not self.path_fn: raise Exception('Unknown routing protocol')
+
+    def generate_rtable(self):
         self.rtable = self.generate_dports(topo, path_fn)
 
-    def k_shortest_paths(self, g, src, dst):
-        paths = list(nx.all_simple_paths(g, src, dst))
-        return sorted(paths, key=lambda p: len(p))
+    def k_shortest_paths(self, g, src, dst, k=8):
+        return yens.k_shortest_paths(g, src, dst, k)[1]
 
-    def ecmp_paths(self, g, src, dst):
-        return list(nx.all_shortest_paths(g, src, dst))
+    def ecmp_paths(self, g, src, dst, k=8):
+        paths = list(nx.all_shortest_paths(g, src, dst))
+        if len(paths) > k: paths = paths[:k]
+        return paths
 
     # Creates routing table:
     # switch_id -> (node_id -> [egress ports])
@@ -33,18 +48,48 @@ class Routing():
             port_map[l[0]][l[1]] = l[2]['port1']
             port_map[l[1]][l[0]] = l[2]['port2']
 
-        switches = topo.switches()
+        switches = self.topo.switches()
         g = nx.Graph()
-        g.add_nodes_from(topo.nodes())
-        g.add_edges_from(topo.links())
+        g.add_nodes_from(self.topo.nodes())
+        g.add_edges_from(self.topo.links())
 
         # find paths from each switch to each node
         for src in switches:
             for dst in filter(lambda n: n != src, topo.nodes()):
-                paths = path_fn(g, src, dst)
-                # all protos use 8 as max paths for now
-                if len(paths) > 8: paths = paths[:8]
+                paths = self.path_fn(g, src, dst)
                 # convert paths to egress ports
                 rtable[src][dst] = [port_map[src][p[1]] for p in paths]
 
         return rtable
+
+    # generates paths for random permutation traffic
+    # (each host connected to only one other host)
+    def generate_random_permutation_paths(self):
+        all_paths = []
+
+        hosts = self.topo.hosts()
+        g = nx.Graph()
+        g.add_nodes_from(self.topo.nodes())
+        g.add_edges_from(self.topo.links())
+
+        # find paths between pairs of nodes (that have paths)
+        pairs = itertools.combinations(hosts, 2)
+        pairs = filter(lambda p: nx.has_path(g, *p), pairs)
+
+        while pairs:
+            src, dst = random.choice(pairs)
+            # remove all pairs connected to nodes that already have paths
+            pairs = filter(lambda p: src not in p and dst not in p, pairs)
+            paths = self.path_fn(g, src, dst)
+            all_paths.extend(paths)
+
+        return all_paths
+
+    # returns a map from link to the number of distinct paths that link is on
+    def edge_counts(self, paths):
+        edge_counts = { tuple(sorted(e)): 0 for e in self.topo.links() }
+        for p in paths:
+            edges = [tuple(sorted([p[i], p[i+1]])) for i in range(len(p) - 1)]
+            for e in edges:
+                edge_counts[e] += 1
+        return edge_counts
--- pox/pox/ext/topologies.py
+++ pox/pox/ext/topologies.py
@@ -1,7 +1,6 @@
-
-import itertools
-import random
 from mininet.topo import Topo
+from collections import defaultdict
+import random
 
 """
 Keep all topology definitions for PA2 in this file.
@@ -16,9 +15,11 @@ class JellyfishTopo(Topo):
     r of which are connected to other switches
     They are connected to each other using the jellyfish algorithm
     """
-    def build(self, n=4, k=NUM_PORTS, r=2):
+    def build(self, n=4, k=NUM_PORTS, r=None):
         if r is None: r = k-1
         self.switch_ports_remaining = dict()
+        self.remaining_switches = [] # switches that have >1 port left
+        self.connections = defaultdict(lambda: []) # switch -> list of connected switches
         self.temp_links = []
 
         # add n switches, each attached to k hosts
@@ -32,6 +33,7 @@ class JellyfishTopo(Topo):
             #       however, switch numbers don't matter, only the number of
             #       remaining switches, so this seemed simpler.
             self.switch_ports_remaining[s] = r
+            self.remaining_switches.append(s)
 
         # run jellyfish to connect switches to one another
         self.make_jellyfish_topo()
@@ -42,35 +44,39 @@ class JellyfishTopo(Topo):
     def make_jellyfish_topo(self):
         # while possible, join two random switches with free ports
         while True:
-            avail = self.find_available_pairs()
-            if not avail: break
-            p = avail[0]
+            p = self.find_available_pair()
+            if not p: break
             self.temp_links.append(p)
-            self.switch_ports_remaining[p[0]] -= 1
-            self.switch_ports_remaining[p[1]] -= 1
+            self.connections[p[0]].append(p[1])
+            self.connections[p[1]].append(p[0])
+            for s in p:
+                self.switch_ports_remaining[s] -= 1
+                if self.switch_ports_remaining[s] == 0:
+                    self.remaining_switches.remove(s)
 
         # if a switch remains with >=2 free ports (p1, p2),
         # pick a random existing link (x, y), remove it,
         # and connect (p1, x) and (p2, y)
-        remain = filter(lambda s: self.switch_ports_remaining[s] >= 2, self.switches())
-        if remain:
-            s = remain[0]
-            neighbors = filter(lambda l: s in l, self.temp_links)
-            neighbors = [filter(lambda l: l != s, l)[0] for l in neighbors]
-            possible = filter(lambda l: s not in l and l[0] not in neighbors \
-                and l[1] not in neighbors, self.temp_links)
+        if self.remaining_switches:
+            s = self.remaining_switches[0]
+            connected = self.connections[s]
+            # the switch must already be connected to neither switch
+            possible = filter(lambda l: l[0] not in connected and l[1] not in connected,
+                self.temp_links)
             if possible:
                 l = possible[0]
                 self.temp_links.remove(l)
                 self.temp_links.append((s, l[0]))
                 self.temp_links.append((s, l[1]))
 
-    def find_available_pairs(self):
-        avail = filter(lambda s: self.switch_ports_remaining[s] > 0, self.switches())
-        pairs = itertools.combinations(avail, 2)
-        pairs = filter(lambda p: p not in self.temp_links, pairs)
-        random.shuffle(pairs)
-        return pairs
+    def find_available_pair(self):
+        random.shuffle(self.remaining_switches) # this is slow, but necessary...
+        for s1 in self.remaining_switches:
+            for s2 in self.remaining_switches:
+                if s1 == s2: continue
+                if s2 in self.connections[s1]: continue
+                return (s1, s2)
+        return None
 
     def generate_rtable(self):
         """
@@ -102,7 +108,6 @@ class DummyTopo(Topo):
         self.addLink( leftHost, switch )
         self.addLink( switch, rightHost )
 
-        return
 
 class FatTreeTopo(Topo):
     """
