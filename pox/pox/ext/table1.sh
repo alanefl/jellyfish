5b6c50315033a1906392307fdfcb11b7bf0f3c28
Alan <alanf94@stanford.edu>
2018-05-09 00:46:51 -0700

    forgot seed

--- pox/pox/ext/table1.sh
+++ pox/pox/ext/table1.sh
@@ -7,7 +7,7 @@ echo '**** Running test: random permutation traffic, 1 TCP flow, ECMP ****'
 
 # 1. Run random permutation traffic test on Jellyfish topology with
 #    n=25, k=4, r=3. With congestion control: TCP 1 flows, Routing: ECMP
-sudo python run.py -randpermtraffic --flows 1 -t jelly,25,4,3 --routing ecmp
+sudo python run.py -randpermtraffic --flows 1 -t jelly,25,4,3 --routing ecmp --seed 0
 
 exit
 
@@ -15,16 +15,16 @@ echo '**** Running test: random permutation traffic, 8 TCP flows, ECMP ****'
 
 # 2. Run random permutation traffic test on Jellyfish topology with
 #    n=25, k=4, r=3. With congestion control: TCP 8 flows, Routing: ECMP
-sudo python run.py -randpermtraffic --flows 8 -t jelly,25,4,3 --routing ecmp
+sudo python run.py -randpermtraffic --flows 8 -t jelly,25,4,3 --routing ecmp --seed 0
 
 echo '**** Running test: random permutation traffic, 1 TCP flow, KSHORT ****'
 
 # 3. Run random permutation traffic test on Jellyfish topology with
 #    n=25, k=4, r=3. With congestion control: TCP 1 flows, Routing: ECMP
-sudo python run.py -randpermtraffic --flows 1 -t jelly,10,4,3 --routing kshort
+sudo python run.py -randpermtraffic --flows 1 -t jelly,10,4,3 --routing kshort --seed 0
 
 echo '**** Running test: random permutation traffic, 8 TCP flows, KSHORT ****'
 
 # 4. Run random permutation traffic test on Jellyfish topology with
 #    n=25, k=4, r=3. With congestion control: TCP 8 flows, Routing: ECMP
-sudo python run.py -randpermtraffic --flows 8 -t jelly,10,4,3 --routing kshort
+sudo python run.py -randpermtraffic --flows 8 -t jelly,10,4,3 --routing kshort --seed 0
